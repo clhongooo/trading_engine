52c0609475599deea49d5b14e713a0bcf4bbb4be
Sunny Yan <sunny.yan@cashalgo.com>
2016-01-26 18:21:02 +0800

    WIP

---
+++ CorrelationCalculator/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lNirvanaCommon -lboost_date_time -lboost_thread -lboost_system -lboost_iostreams -lgsl -lm -ldl -lgslcblas
+
