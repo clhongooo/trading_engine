b86ea8e35f6c69a3052d23e2fd0e8bc5825e3d70
Sunny Yan <sunny.yan@cashalgo.com>
2016-01-27 17:39:03 +0800

    WIP

---
+++ loglibrary/Debug/src/subdir.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/ATU_Feed_Logger.cpp \
+../src/ATU_Logger.cpp \
+../src/ATU_Timer.cpp \
+../src/LogStream.cpp 
+
+OBJS += \
+./src/ATU_Feed_Logger.o \
+./src/ATU_Logger.o \
+./src/ATU_Timer.o \
+./src/LogStream.o 
+
+CPP_DEPS += \
+./src/ATU_Feed_Logger.d \
+./src/ATU_Logger.d \
+./src/ATU_Timer.d \
+./src/LogStream.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -D '__SVN_REV__="$(shell svnversion -n /home/qy/Dropbox/nirvana/workspaceCN/loglibrary)"' -I"/home/qy/Dropbox/nirvana/workspaceCN/loglibrary/include" -I"/home/qy/Dropbox/nirvana/workspaceCN/loglibrary/src" -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
