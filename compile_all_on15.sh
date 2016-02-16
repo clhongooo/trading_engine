1518544593f60777a62d0afcec840f07ff21b55a
Sunny Yan <sunny.yan@cashalgo.com>
2016-02-16 12:06:28 +0800

    WIP

---
+++ compile_all_on15.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+LSDIRO="/home/qy/Dropbox/nirvana/workspaceCN/loglibrary/Debug \
+/home/qy/Dropbox/nirvana/workspaceCN/atu_tcp_library/Debug \
+/home/qy/Dropbox/nirvana/workspaceCN/NirvanaCommon/Debug \
+/home/qy/Dropbox/nirvana/workspaceCN/NirvanaInfrastructure/Debug \
+/home/qy/Dropbox/nirvana/workspaceCN/TradingEngine/Debug"
+
+for DIRO in $LSDIRO
+do
+    cd $DIRO
+    make
+done
