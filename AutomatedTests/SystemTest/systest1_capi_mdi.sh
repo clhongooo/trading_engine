268254c936892b51fd25e7049dff16e532368ef5
Sunny Yan <sunny.yan@cashalgo.com>
2017-04-09 20:36:07 +0800

    WIP

--- AutomatedTests/SystemTest/systest1_capi_mdi.sh
+++ AutomatedTests/SystemTest/systest1_capi_mdi.sh
@@ -6,9 +6,10 @@ sleep 1
 
 if [[ $1 == *"d"* ]]
 then
-    gdb --args ../../TradingEngine/Debug/TradingEngine systest1_config.ini
+    gdb --args $HOME/TradingEngineBuild/TradingEngine systest1_config.ini
 else
-    ../../TradingEngine/Debug/TradingEngine systest1_config.ini
+    $HOME/TradingEngineBuild/TradingEngine systest1_config.ini
 fi
 
+sleep 1
 source kill_simulation_capi.sh
