71b725c0be17454c42f7acef5aded2775e6d4c85
Sunny Yan <sunny.yan@cashalgo.com>
2017-04-13 11:41:43 +0800

    chg indentation

--- bin/run_cme_mdp_cert_test.sh
+++ bin/run_cme_mdp_cert_test.sh
@@ -5,6 +5,6 @@ if [[ $1 == "d" ]]
 then
     gdb --args $BIN 'multicast://224.0.25.126:16901|10.249.71.131' $CMEInstrumentsFile CNHH7 CNHM7
 else
-    $BIN 'multicast://224.0.25.126:16901|10.249.71.131' $CMEInstrumentsFile CNHH7 CNHM7
-    # $BIN 'multicast://224.0.28.125:16644|10.249.71.131' $CMEInstrumentsFile CNHH7 CNHM7
+    # $BIN 'multicast://224.0.25.126:16901|10.249.71.131' $CMEInstrumentsFile CNHH7 CNHM7
+    $BIN 'multicast://224.0.28.125:16644|10.249.71.131' $CMEInstrumentsFile CNHH7 CNHM7
 fi
--- src/main/cme_mdp/CmeMdApi.h
+++ src/main/cme_mdp/CmeMdApi.h
@@ -9,58 +9,58 @@
 #include <MessageHeader.h>
 
 class CmeMdApi {
-public:
-	virtual ~CmeMdApi() = default;
+  public:
+    virtual ~CmeMdApi() = default;
 
-	virtual void Init() = 0;
-	void SubscribeMarketData(const std::vector<std::string>& instruments);
-	void LoadInstrumentsConfig(const std::map<int, std::string>& config_map);
+    virtual void Init() = 0;
+    void SubscribeMarketData(const std::vector<std::string>& instruments);
+    void LoadInstrumentsConfig(const std::map<int, std::string>& config_map);
 
-	struct level {
-		int64_t price;
-		int size;
-		int norder;
-	};
+    struct level {
+      int64_t price;
+      int size;
+      int norder;
+    };
 
-	struct OrderBook {
-		uint64_t transact_time;
-		int seqno;
-		int secid;
-		struct level bid[10], ask[10];
-		char instid[16];
-	};
+    struct OrderBook {
+      uint64_t transact_time;
+      int seqno;
+      int secid;
+      struct level bid[10], ask[10];
+      char instid[16];
+    };
 
-protected:
-	struct binhdr {
-		uint32_t msg_seqno;
-		uint64_t sending_time;
-	};
+  protected:
+    struct binhdr {
+      uint32_t msg_seqno;
+      uint64_t sending_time;
+    };
 
-	virtual void OnRefreshBook(const OrderBook *ob) = 0;
+    virtual void OnRefreshBook(const OrderBook *ob) = 0;
 
-	void OnRefreshBook(mktdata::MessageHeader& hdr, char *buf, int len);
-	void OnInstrumentDefinitionOption(mktdata::MessageHeader& hdr, char *buf, int len);
-	void OnRefreshVolume(mktdata::MessageHeader& hdr, char *buf, int len);
-	void OnRefreshTrade(mktdata::MessageHeader& hdr, char *buf, int len);
-	void OnRefreshTradeSummary(mktdata::MessageHeader& hdr, char *buf, int len);
-	void OnRefreshDailyStatistics(mktdata::MessageHeader& hdr, char *buf, int len);
-	void OnRefreshSessionStatistics(mktdata::MessageHeader& hdr, char *buf, int len);
-	void OnRefreshLimitsBanding(mktdata::MessageHeader& hdr, char *buf, int len);
-	void OnRefreshSecurityDefinitionFuture(mktdata::MessageHeader& hdr, char *buf, int len);
-	void OnRefreshSecurityDefinitionSpread(mktdata::MessageHeader& hdr, char *buf, int len);
-	void OnQuoteRequest(mktdata::MessageHeader& hdr, char *buf, int len);
-	void OnSecurityStatus(mktdata::MessageHeader& hdr, char *buf, int len);
-	void OnChannelReset(mktdata::MessageHeader& hdr, char *buf, int len);
-	void OnSnapshotFullRefresh(mktdata::MessageHeader& hdr, char *buf, int len);
+    void OnRefreshBook(mktdata::MessageHeader& hdr, char *buf, int len);
+    void OnInstrumentDefinitionOption(mktdata::MessageHeader& hdr, char *buf, int len);
+    void OnRefreshVolume(mktdata::MessageHeader& hdr, char *buf, int len);
+    void OnRefreshTrade(mktdata::MessageHeader& hdr, char *buf, int len);
+    void OnRefreshTradeSummary(mktdata::MessageHeader& hdr, char *buf, int len);
+    void OnRefreshDailyStatistics(mktdata::MessageHeader& hdr, char *buf, int len);
+    void OnRefreshSessionStatistics(mktdata::MessageHeader& hdr, char *buf, int len);
+    void OnRefreshLimitsBanding(mktdata::MessageHeader& hdr, char *buf, int len);
+    void OnRefreshSecurityDefinitionFuture(mktdata::MessageHeader& hdr, char *buf, int len);
+    void OnRefreshSecurityDefinitionSpread(mktdata::MessageHeader& hdr, char *buf, int len);
+    void OnQuoteRequest(mktdata::MessageHeader& hdr, char *buf, int len);
+    void OnSecurityStatus(mktdata::MessageHeader& hdr, char *buf, int len);
+    void OnChannelReset(mktdata::MessageHeader& hdr, char *buf, int len);
+    void OnSnapshotFullRefresh(mktdata::MessageHeader& hdr, char *buf, int len);
 
-	void Disseminate(char *buf, int len);
-	void ResetBooks();
+    void Disseminate(char *buf, int len);
+    void ResetBooks();
 
-	const int ver = 6;
-	FILE *fp;
-	std::set<std::string> subscribe;
-	std::map<std::string, OrderBook> obmap;
-	std::map<int, std::string> instmap;
+    const int ver = 6;
+    FILE *fp;
+    std::set<std::string> subscribe;
+    std::map<std::string, OrderBook> obmap;
+    std::map<int, std::string> instmap;
 
-	std::string TransferTimestamp(uint64_t micro_sec);
+    std::string TransferTimestamp(uint64_t micro_sec);
 };
