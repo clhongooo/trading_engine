4e0a2fa2283a82273a796e351c1d486a3cabd11d
Sunny Yan <sunny.yan@cashalgo.com>
2017-04-19 09:22:01 +0800

    added options in build script

--- buildProj.sh
+++ buildProj.sh
@@ -3,25 +3,19 @@
 CORE_MINUS_1=$(cat /proc/cpuinfo | grep processor | tail -n 1 | awk '{print $3}')
 TARGET_FOLDER=target
 BIN_FOLDER=bin
-if [[ $@ == *"c"* && -d $TARGET_FOLDER ]]
-then
-    rm -rf $TARGET_FOLDER
-fi
 [[ ! -d $TARGET_FOLDER ]] && mkdir $TARGET_FOLDER
 [[ ! -d $BIN_FOLDER ]] && mkdir $BIN_FOLDER
 cd $TARGET_FOLDER
 
-if [[ $@ == *"d"* ]]
+PROJ=""
+if [[ -n $2 ]]
 then
-    cmake -DCMAKE_BUILD_TYPE=Debug ../src/main/
-elif [[ $@ == *"r"* ]]
-then
-    cmake -DCMAKE_BUILD_TYPE=Release ../src/main/
+    $PROJ=$2
 fi
 MAKE_VERBOSITY=""
 if [[ $@ == *"v"* ]]
 then
     MAKE_VERBOSITY="VERBOSE=1"
 fi
-make -j $CORE_MINUS_1 $MAKE_VERBOSITY
+make -j $CORE_MINUS_1 $MAKE_VERBOSITY $PROJ
 find . -type f -executable | grep -v CMakeFiles | xargs -I{} cp {} ../$BIN_FOLDER
