f85c73bfde54e24fc58f3ec4d0bbc2fc139ce54c
Sunny Yan <sunny.yan@cashalgo.com>
2017-04-27 22:56:55 +0800

    try MDP config

--- bin/run_cme_mdp_cert_test.sh
+++ bin/run_cme_mdp_cert_test.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-BIN=cme_mdp
-CMEInstrumentsFile=../conf/cme_mdp/CMEInstruments.cfg
+# BIN=cme_mdp
+# CMEInstrumentsFile=../conf/cme_mdp/CMEInstruments.cfg
+# if [[ $1 == "d" ]]
+# then
+#     gdb --args $BIN 'multicast://224.0.25.126:16901|10.249.71.131' $CMEInstrumentsFile CNHH7 CNHM7
+# else
+#     $BIN 'multicast://224.0.28.123:16657|10.249.71.131' $CMEInstrumentsFile CNHH7 CNHM7
+# fi
+BIN=omd_mdp
+CONFIG=../conf/cme_mdp/Config_MDP_nrcert.ini
 if [[ $1 == "d" ]]
 then
-    gdb --args $BIN 'multicast://224.0.25.126:16901|10.249.71.131' $CMEInstrumentsFile CNHH7 CNHM7
+    gdb --args $BIN $CONFIG
 else
-    $BIN 'multicast://224.0.28.123:16657|10.249.71.131' $CMEInstrumentsFile CNHH7 CNHM7
+    $BIN $CONFIG
 fi
