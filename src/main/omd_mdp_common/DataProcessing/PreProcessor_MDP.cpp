2d27a66a74a2000186fd66b50ee6cbdf05d28ac2
Sunny Yan <sunny.yan@cashalgo.com>
2017-05-03 22:21:56 +0800

    chg log print

--- src/main/omd_mdp_common/DataProcessing/PreProcessor_MDP.cpp
+++ src/main/omd_mdp_common/DataProcessing/PreProcessor_MDP.cpp
@@ -51,16 +51,28 @@ void PreProcessor_MDP::Run()
     //--------------------------------------------------
     // Output other Debug info
     //--------------------------------------------------
-    m_Logger->Write(Logger::DEBUG,"PreProcessor: %s : %u : m_RawPktCirBuf.Size()          %u", m_McastIdentifier.IP().c_str(), m_McastIdentifier.Port(), m_RawPktCirBuf->Size());
-    m_Logger->Write(Logger::DEBUG,"PreProcessor: %s : %u : m_RawPktCirBuf.AllocatedSize() %u", m_McastIdentifier.IP().c_str(), m_McastIdentifier.Port(), m_RawPktCirBuf->AllocatedSize());
-    m_Logger->Write(Logger::DEBUG,"PreProcessor: %s : %u : m_RawPktCirBuf.GetPktSize()    %u", m_McastIdentifier.IP().c_str(), m_McastIdentifier.Port(), m_RawPktCirBuf->GetPktSize());
+    m_Logger->Write(Logger::DEBUG,"PreProcessor: %s : %u ChannelID:%u. %s(%c): m_RawPktCirBuf.Size()          %u", m_McastIdentifier.IP().c_str(), m_McastIdentifier.Port(), m_ChannelID, (m_McastIdentifier.McastType() == McastIdentifier::REALTIME ? "RT" : "RF"), (m_McastIdentifier.Channel() == McastIdentifier::A ? 'A':'B'), m_RawPktCirBuf->Size());
+    m_Logger->Write(Logger::DEBUG,"PreProcessor: %s : %u ChannelID:%u. %s(%c): m_RawPktCirBuf.AllocatedSize() %u", m_McastIdentifier.IP().c_str(), m_McastIdentifier.Port(), m_ChannelID, (m_McastIdentifier.McastType() == McastIdentifier::REALTIME ? "RT" : "RF"), (m_McastIdentifier.Channel() == McastIdentifier::A ? 'A':'B'), m_RawPktCirBuf->AllocatedSize());
+    m_Logger->Write(Logger::DEBUG,"PreProcessor: %s : %u ChannelID:%u. %s(%c): m_RawPktCirBuf.GetPktSize()    %u", m_McastIdentifier.IP().c_str(), m_McastIdentifier.Port(), m_ChannelID, (m_McastIdentifier.McastType() == McastIdentifier::REALTIME ? "RT" : "RF"), (m_McastIdentifier.Channel() == McastIdentifier::A ? 'A':'B'), m_RawPktCirBuf->GetPktSize());
 
     //--------------------------------------------------
     // Output Packet Header info
     //--------------------------------------------------
     MDP_Packet_Header * mph  = (MDP_Packet_Header*)(pbPkt);
-    m_Logger->Write(m_PrintPreProcSeqNoAsInfo ? Logger::INFO : Logger::DEBUG, "PreProcessor: ChannelID:%u. %s(%c): Packet Header: PktSeqNum:  %u", m_ChannelID, (m_McastIdentifier.McastType() == McastIdentifier::REALTIME ? "RT" : "RF"), (m_McastIdentifier.Channel() == McastIdentifier::A ? 'A':'B'), mph->PktSeqNum);
-    m_Logger->Write(Logger::DEBUG,"PreProcessor: ChannelID:%u. %s(%c): Packet Header: Send Time:  %s", m_ChannelID, (m_McastIdentifier.McastType() == McastIdentifier::REALTIME ? "RT" : "RF"), (m_McastIdentifier.Channel() == McastIdentifier::A ? 'A':'B'), SDateTime::fromUnixTimeToString(mph->SendingTime, SDateTime::NANOSEC).c_str());
+    m_Logger->Write(m_PrintPreProcSeqNoAsInfo ? Logger::INFO : Logger::DEBUG, "PreProcessor: %s : %u ChannelID:%u. %s(%c): Packet Header: PktSeqNum:  %u",
+                    m_McastIdentifier.IP().c_str(),
+                    m_McastIdentifier.Port(),
+                    m_ChannelID,
+                    (m_McastIdentifier.McastType() == McastIdentifier::REALTIME ? "RT" : "RF"),
+                    (m_McastIdentifier.Channel() == McastIdentifier::A ? 'A':'B'),
+                    mph->PktSeqNum);
+    m_Logger->Write(Logger::DEBUG,"PreProcessor: %s : %u ChannelID:%u. %s(%c): Packet Header: Send Time:  %s",
+                    m_McastIdentifier.IP().c_str(),
+                    m_McastIdentifier.Port(),
+                    m_ChannelID,
+                    (m_McastIdentifier.McastType() == McastIdentifier::REALTIME ? "RT" : "RF"),
+                    (m_McastIdentifier.Channel() == McastIdentifier::A ? 'A':'B'),
+                    SDateTime::fromUnixTimeToString(mph->SendingTime,SDateTime::NANOSEC).c_str());
 
     //--------------------------------------------------
     // We replace the original packet header timestamp with the packet length, so that RealTimeProcessor knows how long the packet is.
