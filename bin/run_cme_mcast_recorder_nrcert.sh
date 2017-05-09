b48c394ec403426d710ee40c067a687a3cdd49af
Sunny Yan <sunny.yan@cashalgo.com>
2017-05-09 21:33:19 +0800

    alt config

--- bin/run_cme_mcast_recorder_nrcert.sh
+++ bin/run_cme_mcast_recorder_nrcert.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+# CONFIG=../conf/mcast_recorder/cme_mdp/ncert.csv
+CONFIG=../conf/mcast_recorder/cme_mdp/ncert_snapshot.csv
 rm -f cme_mdp_data/*
 if [[ $1 == "d" ]]
 then
-    gdb --args mcast_recorder ../conf/mcast_recorder/cme_mdp/ncert.csv cme_mdp_data 3
+    gdb --args mcast_recorder $CONFIG cme_mdp_data 3
 else
-    mcast_recorder ../conf/mcast_recorder/cme_mdp/ncert.csv cme_mdp_data 3
+    mcast_recorder $CONFIG cme_mdp_data 3
 fi
