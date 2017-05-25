94e4972b305984fa506e2c53f10d04759c8458c9
Sunny Yan <sunny.yan@cashalgo.com>
2017-05-25 17:23:04 +0800

    sorted

--- scripts/capi/simple_mdi_client.py
+++ scripts/capi/simple_mdi_client.py
@@ -96,7 +96,7 @@ recv_thd.start()
 while True:
     print BBlack + ("%s" % time.strftime("%Y-%m-%d %H:%M:%S")) + Color_Off
 
-    for k,v in MD_DICT.items():
+    for k,v in sorted(MD_DICT.items()):
         print BBlue + str(k) + " " + Color_Off + right_justify("T: ",5) + right_justify(v[0],10) + right_justify("B/A: ",10) + ' '.join(map(lambda x: right_justify(x,10),v[1:]))
 
     time.sleep(1)
