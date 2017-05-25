28c091e9426f2c823a7e1928aec63eec733d2b92
Sunny Yan <sunny.yan@cashalgo.com>
2017-05-25 15:18:04 +0800

    more formatting

--- scripts/order_routing/parse_capi_feeds.py
+++ scripts/order_routing/parse_capi_feeds.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 import sys
 
-def parse_capi_feed(capi_feed):
+def parse_capi_feed(capi_feed,indentation=0):
 
   def justify_str(s,totlen,left_right="right",padchar=' '):
       def extra(s,totlen):
@@ -58,9 +58,12 @@ def parse_capi_feed(capi_feed):
     value_list = ["signalfeed",fields[0],fields[2],fields[3],fields[4],fields[5],fields[6],fields[7],fields[8],fields[9],fields[10],fields[11],fields[12]]
   elif fields[1] == "orderfeed":
     key_list = ["feed_type","timestamp","market","symbol","order_id","price","quantity","open_close","buy_sell","quantity_filled","deleted","order_status","error_desc","source","order_type","order_validity"]
-    value_list = ["orderfeed",fields[0],fields[2],fields[3],fields[4],fields[5],fields[6],fields[7],explain_buy_sell(fields[8]),fields[9],explain_deleted(fields[10]),explain_ordert_status(fields[11]),fields[12],fields[13],fields[14],fields[15],fields[16]]
+    value_list = ["orderfeed",fields[0],fields[2],fields[3],fields[4],fields[5],fields[6],fields[7],explain_buy_sell(fields[8]),fields[9],explain_deleted(fields[10]),explain_ordert_status(fields[11]),fields[12],explain_source(fields[13]),fields[14],fields[15],fields[16]]
+  elif fields[1] == "tradefeed":
+    key_list = ["feed_type","timestamp","market","symbol","order_id","price","quantity","buy_sell","trade_id","source"]
+    value_list = ["tradefeed",fields[0],fields[2],fields[3],fields[4],fields[5],fields[6],explain_buy_sell(fields[7]),fields[8],explain_source(fields[9])]
 
-  return map(lambda x: right_justify(x[0],20) + " : " + left_justify(x[1],30), zip(key_list,value_list))
+  return map(lambda x: right_justify(x[0],indentation+20) + " : " + left_justify(x[1],30), zip(key_list,value_list))
 
 if __name__ == '__main__':
   in_lines = [line for line in sys.stdin]
