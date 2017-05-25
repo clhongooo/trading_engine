489f9541f9624e7ae9673eee247c2929dc0662ad
Sunny Yan <sunny.yan@cashalgo.com>
2017-05-25 15:18:32 +0800

    better look

--- scripts/order_routing/simple_order_placement_client.py
+++ scripts/order_routing/simple_order_placement_client.py
@@ -8,6 +8,31 @@ from threading import Thread
 
 JUSTIFY_COL=200
 
+###################################################
+Color_Off="\033[0m"       # Text Reset
+
+# Regular Colors
+Black="\033[0;30m"        # Black
+Red="\033[0;31m"          # Red
+Green="\033[0;32m"        # Green
+Yellow="\033[0;33m"       # Yellow
+Blue="\033[0;34m"         # Blue
+Purple="\033[0;35m"       # Purple
+Cyan="\033[0;36m"         # Cyan
+White="\033[0;37m"        # White
+
+# Bold
+BBlack="\033[1;30m"       # Black
+BRed="\033[1;31m"         # Red
+BGreen="\033[1;32m"       # Green
+BYellow="\033[1;33m"      # Yellow
+BBlue="\033[1;34m"        # Blue
+BPurple="\033[1;35m"      # Purple
+BCyan="\033[1;36m"        # Cyan
+BWhite="\033[1;37m"       # White
+###################################################
+
+
 def read_file(file_loc):
     with open(file_loc,'r') as f:
         return [line for line in f]
@@ -29,10 +54,13 @@ def left_justify(s,totlen):
     return justify_str(s,totlen,"left")
 
 def get_exchg(sym):
-    if sym[:3] in ["HSI","HHI","MHI","MCH"]:
+    sym_up = sym.upper()
+    if sym_up[:3] in ["HSI","HHI","MHI","MCH"]:
         return "HKFE"
-    elif sym[:2] in ["ES","NQ","YM","GC","SI"]:
+    elif sym_up[:2] in ["ES","NQ","YM","GC","SI","HG"]:
         return "CME"
+    elif sym_up[:2] in ["AU","AG","CU"]:
+        return "SHFE"
     else:
         return "HKFE"
 
@@ -46,14 +74,15 @@ def expand_to_signalfeed(oid_sf_tup_dict,fields_list,oid_suffix=0):
         new_sf[9] = "delete"
         return construct_tup(new_sf)
     else:
-        oaction,sym,px,qty = fields_list
+        augmented_fields_list = (fields_list + [""]*10)[:5]
+        oaction,sym,px,qty,openclose = augmented_fields_list
 
         exchg         = get_exchg(sym)
         ymdhms        = time.strftime("%Y%m%d_%H%M%S")
         hms           = time.strftime("%H%M%S")
         fts           = ymdhms + "_000000"
         oid           = '_'.join(map(str, ["oid",hms,oid_suffix]))
-        open_or_close = "open"
+        open_or_close = "open" if openclose == "" else openclose
         buy_or_sell   = "1" if float(qty) >= 0 else "2"
         oaction       = "insert" if oaction.lower() == "i" else "delete"
         otype         = "limit_order"
@@ -66,13 +95,13 @@ def recv_zmq():
     global zmq_socket
     while True:
         m = zmq_socket.recv()
-        print right_justify("Received: %s" % m, JUSTIFY_COL)
-        print '\n'.join(parse_capi_feed(m))
+        print right_justify("Received: %s%s%s" % (BYellow,m,Color_Off), JUSTIFY_COL)
+        print '\n'.join(parse_capi_feed(m,100))
 
 def send_zmq(msg):
     global zmq_socket
     zmq_socket.send(msg)
-    print right_justify("Sent: %s" % msg, JUSTIFY_COL)
+    print right_justify("Sent: %s%s%s" % (BYellow,msg,Color_Off), JUSTIFY_COL)
 
 ###################################################
 parser = argparse.ArgumentParser(description = "Connects to ZMQ server and places orders by constructing signal feed upon user input.")
@@ -98,6 +127,7 @@ signalfeed_dict = dict(map(lambda x: (x[0][4], (x[0],x[1])), signalfeed_list))
 def recursion(oid_sf_tup_dict):
     print '\n'.join(map(lambda x: right_justify(x[0],8) + "   " + x[1], enumerate(map(lambda x: oid_sf_tup_dict[x][1], sorted(map(lambda x: x[0][4], oid_sf_tup_dict.values()))))))
     user_input = raw_input("command [s oid | i,sym,price,qty | d,order_id | q]\n").strip()
+    print BRed + user_input + Color_Off
     if len(user_input) == 0:
         recursion(oid_sf_tup_dict)
     if user_input[0] == "q":
