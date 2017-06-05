a9ab20de629c4c318cc87a6dcb30d734ba7e6c47
Sunny Yan <sunny.yan@cashalgo.com>
2017-06-05 12:09:11 +0800

    strip null char

--- scripts/capi/parse_capi_feeds.py
+++ scripts/capi/parse_capi_feeds.py
@@ -75,19 +75,19 @@ def parse_capi_feed(capi_feed,indentation=0):
 
   key_list   = []
   value_list = []
-  fields = capi_feed.strip().split(',') + [""]*10
+  fields = capi_feed.strip('\0').strip().split(',') + [""]*10
   if fields[1] == "signalfeed":
-    key_list = ["feed_type","timestamp","market","symbol","order_id","price","quantity","open_close","buy_sell","order_action","order_type","order_validity","others"]
-    value_list = ["signalfeed",fields[0],fields[2],fields[3],fields[4],fields[5],fields[6],fields[7],fields[8],fields[9],fields[10],fields[11],fields[12]]
+      key_list = ["feed_type","timestamp","market","symbol","order_id","price","quantity","open_close","buy_sell","order_action","order_type","order_validity","others"]
+      value_list = ["signalfeed",fields[0],fields[2],fields[3],fields[4],fields[5],fields[6],fields[7],fields[8],fields[9],fields[10],fields[11],fields[12]]
   elif fields[1] == "orderfeed":
-    key_list = ["feed_type","timestamp","market","symbol","order_id","price","quantity","open_close","buy_sell","quantity_filled","deleted","order_status","error_desc","source","order_type","order_validity"]
-    value_list = ["orderfeed",fields[0],fields[2],fields[3],fields[4],fields[5],fields[6],fields[7],explain_buy_sell(fields[8]),fields[9],explain_deleted(fields[10]),explain_ordert_status(fields[11]),fields[12],explain_source(fields[13]),fields[14],fields[15],fields[16]]
+      key_list = ["feed_type","timestamp","market","symbol","order_id","price","quantity","open_close","buy_sell","quantity_filled","deleted","order_status","error_desc","source","order_type","order_validity"]
+      value_list = ["orderfeed",fields[0],fields[2],fields[3],fields[4],fields[5],fields[6],fields[7],explain_buy_sell(fields[8]),fields[9],explain_deleted(fields[10]),explain_ordert_status(fields[11]),fields[12],explain_source(fields[13]),fields[14],fields[15],fields[16]]
   elif fields[1] == "tradefeed":
-    key_list = ["feed_type","timestamp","market","symbol","order_id","price","quantity","buy_sell","trade_id","source"]
-    value_list = [BPurple+"tradefeed"+Color_Off,fields[0],fields[2],fields[3],fields[4],fields[5],fields[6],explain_buy_sell(fields[7]),fields[8],explain_source(fields[9])]
+      key_list = ["feed_type","timestamp","market","symbol","order_id","price","quantity","buy_sell","trade_id","source"]
+      value_list = [BPurple+"tradefeed"+Color_Off,fields[0],fields[2],fields[3],fields[4],fields[5],fields[6],explain_buy_sell(fields[7]),fields[8],explain_source(fields[9])]
 
   return map(lambda x: right_justify(x[0],indentation+20) + " : " + left_justify(x[1],30), zip(key_list,value_list))
 
 if __name__ == '__main__':
-  in_lines = [line for line in sys.stdin]
-  print '\n\n'.join(map(lambda x: '\n'.join(parse_capi_feed(x)), in_lines))
+    in_lines = [line for line in sys.stdin]
+    print '\n\n'.join(map(lambda x: '\n'.join(parse_capi_feed(x)), in_lines))
