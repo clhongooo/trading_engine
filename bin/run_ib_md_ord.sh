fd298537dc4dc74a5181a10fef281d4c9f6c474b
somebody@gmail.com <somebody@gmail.com>
2017-07-04 16:19:00 +0800

    create ib order mapping file if necessary

--- bin/run_ib_md_ord.sh
+++ bin/run_ib_md_ord.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 LD_LIBRARY_PATH="../lib:$LD_LIBRARY_PATH"
+CONF_FILE=../conf/ib/ib_md_ord.ini
+
+###################################################
+# create ib order mapping file if necessary
+###################################################
+IB_ORD_MAP_FILE=$(cat $CONF_FILE | grep OrderMappingFile | awk -F= '{print $2}')
+[[ ! -e $IB_ORD_MAP_FILE ]] && touch $IB_ORD_MAP_FILE
 
 if [[ $1 == "d" ]]
 then
-    gdb --args ./ib_md_ord ../conf/ib/ib_md_ord.ini
+    gdb --args ./ib_md_ord $CONF_FILE
 else
-    ./ib_md_ord ../conf/ib/ib_md_ord.ini
+    ./ib_md_ord $CONF_FILE
 fi
